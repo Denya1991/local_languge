c47eb84d8e80077621b6f69c8b812f3232ffbb72
Denya1991 <62621152+Denya1991@users.noreply.github.com>
2021-09-13 13:53:27 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,13 @@
+import time
+
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_check_link_add_to_cart(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form button.btn")
+    browser.get(link)
+    # time.sleep(30)
+    my_list = []
+    element = browser.find_elements_by_css_selector("#add_to_basket_form button.btn")
+    assert element != my_list, "Element not found"
